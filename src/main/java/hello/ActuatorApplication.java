9caa9fc047ba4a304e5ab97fe36768acec899f7c
zidol <ajh3166@naver.com>
2023-07-10 23:57:42 +0900

    메트릭 등록 예제

--- src/main/java/hello/ActuatorApplication.java
+++ src/main/java/hello/ActuatorApplication.java
@@ -1,11 +1,14 @@
 package hello;
 
+import hello.order.v0.OrderServiceV0;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(OrderServiceV0.class)
+@SpringBootApplication(scanBasePackages = "hello.controller")
 public class ActuatorApplication {
 
     public static void main(String[] args) {
